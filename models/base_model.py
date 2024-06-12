75bd0e7e2909cc0a3bb7a34204b8cd89a31051cc
davekilonzo <davidkilonzo1998@gmail.com>
2024-06-12 02:10:03 -0700

    all

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,75 @@
 #!/usr/bin/python3
-
+"""
+Module for the BaseModel class.
+"""
 import uuid
 from datetime import datetime
-from models import storage
+import models
+
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
 
-    def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        models.storage.new(self)
 
     def save(self):
-        self.updated_at = datetime.now()
-        storage.save()
+        """
+
+        """
+        self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
-        dict_copy = self.__dict__.copy()
-        dict_copy['__class__'] = self.__class__.__name__
-        dict_copy['created_at'] = self.created_at.isoformat()
-        dict_copy['updated_at'] = self.updated_at.isoformat()
-        return dict_copy
-    def __init__(self, *args, **kwargs):
-        if kwargs: # if there are keyword arguments
+        """
 
-            for key in kwargs:  # for each key in the dictionary
-                value = kwargs[key]  # get the value for this key
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")  # convert string to datetime
-                elif key == "__class__":
-                    continue  # skip this key
-                setattr(self, key, value)  # set the attribute on the instance
-        else:  # if there are no keyword arguments
-            self.id = str(uuid.uuid4())  # generate a unique ID
-            self.created_at = self.updated_at = datetime.now()
-        if not kwargs:
-            storage.new(self)
+        """
+        inst_dict = self.__dict__.copy()
+        inst_dict["__class__"] = self.__class__.__name__
+        inst_dict["created_at"] = self.created_at.isoformat()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
+
+        return inst_dict
+
+    def __str__(self):
+        """
+
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
 
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My_First_Model"
+    my_model.my_number = 89
+    print(my_model.id)
+    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
